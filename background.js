883bf94aa1c53b98349fa75a2e27e959c0ee8a3f
Maros Meciar <meciar@atlasgroup.cz>
2024-01-05 23:33:20 +0100

    auto

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 const alarmName = "vsb-email-notifier-alarm";
-const queryInfo = {
-  url: "https://posta.vsb.cz/roundcube/?_task=mail&_mbox=INBOX",
-};
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create(alarmName, {
@@ -10,18 +7,22 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.onAlarm.addListener(
     (alarm) =>
       alarm.name === alarmName &&
-      chrome.tabs.query(queryInfo, (tabs) =>
-        tabs.forEach(
-          (tab) =>
-            tab.title.length > 0 &&
-            tab.title[0] === "(" &&
-            chrome.notifications.create({
-              title: "VSB E-mail Notifier",
-              message: "Přišla zpráva!",
-              iconUrl: "images/monkaOMEGA.png",
-              type: "basic",
-            })
-        )
+      chrome.tabs.query(
+        {
+          url: "https://posta.vsb.cz/roundcube/?_task=mail&_mbox=INBOX",
+        },
+        (tabs) =>
+          tabs.forEach(
+            (tab) =>
+              tab.title.length > 0 &&
+              tab.title[0] === "(" &&
+              chrome.notifications.create({
+                title: "VSB E-mail Notifier",
+                message: "Přišla zpráva!",
+                iconUrl: "images/monkaOMEGA.png",
+                type: "basic",
+              })
+          )
       )
   );
 });
